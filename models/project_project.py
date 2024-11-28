e53f57b38b40f9b34eca103831bbec323d4a0458
mbrehumanytek <matias@humanytek.com>
2024-11-28 17:25:10 +0000

    mig to 17.0

--- models/project_project.py
+++ models/project_project.py
@@ -1,10 +1,12 @@
 from odoo import models, fields
 
+
 class ProjectProject(models.Model):
-    _inherit = 'project.project'
-    
-    def _get_all_types(self):
-        return self.env['project.task.type'].search([], limit=5)
+    _inherit = "project.project"
+
+    def _default_task_types(self):
+        # Obtener todos los tipos de tareas disponibles (sin límite específico)
+        return self.env["project.task.type"].search([])
 
     type_ids = fields.Many2many(
         string="Tasks Stages",
@@ -12,5 +14,5 @@ class ProjectProject(models.Model):
         relation="project_task_type_rel",
         column1="project_id",
         column2="type_id",
-        default=_get_all_types,
+        default=_default_task_types,
     )
